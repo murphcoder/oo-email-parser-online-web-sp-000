577368fdeb537daccf6f331bdc6c498ea5dd0d41
Sean Murphy <filmmakersean@fastmail.net>
2019-07-25 15:28:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,7 @@ class EmailParser
   
   def initialize (emails)
     @email_array = emails.split(/(,| )/)
-    @email_array.reject! {|email| 
+    @email_array.reject! {|email| email != 
   end
   
   def parse
